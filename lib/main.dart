00e2bd58a99b03601a8cc5aaaa9f64a350d25302
Soumendra Kumar Sahoo <soumend1@in.ibm.com>
2020-08-22 16:02:34 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first Flutter App"),
+        ),
+        body: Text("This is my default body"),
+      ),
+    );
+  }
+}
